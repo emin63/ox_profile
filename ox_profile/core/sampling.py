53a73fa7a5136dfb8b7fcb864fc12f8b9baabb43
Emin Martinian <emin.martinian@gmail.com>
2018-10-12 16:03:08 -0400

    Added more docs adn doctest

--- ox_profile/core/sampling.py
+++ ox_profile/core/sampling.py
@@ -1,6 +1,7 @@
 """Tools to sample running programs.
 """
 
+import doctest
 import logging
 import sys
 
@@ -9,6 +10,45 @@ from ox_profile.core import metrics
 
 class Sampler(object):
     """Basic class to sample program for statistical profiling.
+
+This is the workhorse for ox_profile to do statistical sampling.
+At a low level, we do this sampling using `sys._current_frames`. As
+suggested by the leading underscore, this system function may be a bit
+less robust. Indeed, the documentation says "This function should be
+used for specialized purposes only." Hopefully the core python
+developers will not make major changes to such a useful function.
+
+The most interesting method is `run` which:
+
+  1. Uses `sys.setswitchinterval` to try and prevent a thread context switch.
+  2. Calls `sys._current_frames` to sample what the python interpreter is
+     doing.
+  3. Updates a simple in-memory database of what functions are running.
+
+In principle, you could just use the Sampler via something like
+
+>>> from ox_profile.core import sampling, recording
+>>> sampler = sampling.Sampler(recording.CountingRecorder())
+>>> def foo():
+...     sampler.run()
+...     return 'done'
+...
+>>> dummy = foo()
+>>> info, calls = sampler.my_db.query()
+>>> pretty = ('%c'%10).join(map(str, info))
+>>> print('%i calls:%c%s' % (calls, 10, pretty)) # doctest: +ELLIPSIS
+1 calls:
+ProfileRecord(...)
+
+The above would have the sampler take a snapshot of the stack frames
+when the `foo` function is run. Of course, this isn't very useful by
+itself because it just tells you that `foo` is being run. It could be
+useful if there were other threads which were running because the
+sampler would tell you what stack frame those threads were in. For
+this reason you should use the SimpleLauncher class in
+the `ox_profile/core/launchers` module which creates an instance
+of this class.
+
     """
 
     def __init__(self, my_db):
@@ -55,3 +95,9 @@ class Sampler(object):
         "Syntactic sugar to call `self.run(*args, **kwargs)`."
 
         return self.run(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    # Run doctest if file executed as a script
+    doctest.testmod()
+    print('Finished Tests')
